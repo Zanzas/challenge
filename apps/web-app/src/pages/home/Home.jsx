6237cc60c822906d40888665535e514b006177e5
tribeiro@auchan.fr <thomasribeiro123456@gmail.com>
2023-11-22 08:43:08 +0100

    feat: add more in homepage

--- apps/web-app/src/pages/home/Home.jsx
+++ apps/web-app/src/pages/home/Home.jsx
@@ -2,16 +2,14 @@ import Button from '../../components/UI/button/button'
 import Header from '../../components/header/Header'
 import Input from '../../components/UI/input/input'
 import './Home.css'
+import FormCreateParkSlot from '../../components/UI/input/input'
 
 export default function home() {
     return (
         <div>
             <Header />
             <div class="window"> 
-                <Input title="Localisation :" placeholder="rue, aéroport" maxLength="50" />
-                <Input title="Début :" placeholder="début"/>
-                <Input title="Fin :" placeholder="fin"/>
-                <Input title="Type de véhicule :" placeholder="citadine, camion"/>
+                <FormCreateParkSlot />
                 <Button/>
             </div>
              
