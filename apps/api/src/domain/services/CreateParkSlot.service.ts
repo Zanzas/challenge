df26407dff0a2c532c3a39425e5b451c5ebaaf4d
tribeiro@auchan.fr <thomasribeiro123456@gmail.com>
2023-12-20 10:07:56 +0100

    feat: add jwt token

--- apps/api/src/domain/services/CreateParkSlot.service.ts
+++ apps/api/src/domain/services/CreateParkSlot.service.ts
@@ -1,15 +1,15 @@
 import { Inject } from '@nestjs/common';
-import { createParkDTO } from 'src/dto/createPark.dto';
+import { CreateParkDTO } from 'src/dto/createPark.dto';
 import { ParkSlot } from 'src/domain/entities/ParkSlot';
 import { ParkSlotRepository } from 'src/domain/interface/ParkSlotRepository.interface';
 
-export class createParkSlot {
+export class CreateParkSlot {
   public constructor(
     @Inject(ParkSlotRepository)
     private parkSlotRepository: ParkSlotRepository,
   ) {}
 
-  public async handle(payload: createParkDTO): Promise<void> {
+  public async handle(payload: CreateParkDTO): Promise<void> {
     const slot = ParkSlot.createByUser(payload);
 
     await this.parkSlotRepository.create(slot);
---
+++ apps/api/src/domain/services/JWT.service.ts
@@ -0,0 +1,5 @@
+// const jwt = require('jsonwebtoken');
+
+// // const token = jwt.sign({ user_id: user._id }, 'votre_secret', {
+// //   expiresIn: '2h', // Expiration du token
+// // });
